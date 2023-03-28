d982b5ceec6b64275722c375d3e4e7b772169fb1
Mathisfranel <mathisfranel@gmail.com>
2023-03-28 17:19:05 +0200

    4eme commit

--- src/main/java/bank/bank.java
+++ src/main/java/bank/bank.java
@@ -4,7 +4,13 @@ public class bank {
     private Account[] accounts;
 
     public bank(){
-        this.accounts = new Account[100];
+        this.accounts = new Account[0];
+    }
+    public int getNbAccounts(){
+        return accounts.length;
+    }
+    public Account[] getAccountsArray(){
+        return accounts;
     }
     public void addAccount(Account account){
         for (int i = 0; i < accounts.length; i++) {
@@ -13,5 +19,9 @@ public class bank {
                 break;
             }
         }
+    }
+    public void transfer(Account account1, Account account2, double montant){
+        account1.retrait(montant);
+        account2.depot(montant);
     }
 }
---
+++ src/test/java/TestBank.java
@@ -0,0 +1,24 @@
+import bank.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.testng.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBank {
+    @DisplayName("Account")
+    @ParameterizedTest(name = "converting {0} should return {1}")
+    @CsvSource({
+            "1, 100, 0.1f"
+    })
+    public void testBank(int accountID, double solde, float taux) {
+        bank bank = new bank();
+        Account account = new Account(accountID, solde, taux);
+        Account account2 = new Account(accountID+1, solde+50, taux);
+        bank.addAccount(account);
+        bank.addAccount(account2);
+        Assert.assertEquals(2, bank.getNbAccounts());
+
+    }
+}
